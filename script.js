318cb2936bf1b38d00ad3f4705a85e328281a874
Viktor Ödman <viktoralexanderodman@gmail.com>
2019-11-04 22:29:33 +0100

    Updated html and more javascript functions

--- script.js
+++ script.js
@@ -44,13 +44,19 @@ function submit() {
 
 
 
+
+    if (answer.value == 'start') {
+        answer.value = '';
+        questionBox.innerHTML = '<span class="text"> Det är en vacker morgon ovanpå sockervaddsmolnet, du har precis gnuggat sömnen ur ögonen när en magisk fräs uppenbarar sig.<br><br> "Här kommer jag med två gåvor!" Vill du ha en <span class="alternativ">potatis</span> eller en <span class="alternativ">gåta</span>?<br></span>'
+    }
+
     // DET STORA POTATIS ÄVENTYRET
 
     if (answer.value == 'potatis') {
         answer.value = '';
 
 
-        questionBox.innerHTML = '<span class="text">Du valde potatis! Mycket bra val!, joddlade jordfräsen. <br><br> Innan du tar en tugga vill jag att du ska veta, att om du går vilse, ropa bara <span class="alternativ">hem</span>. <br> <br>Vill du ta en <span class="alternativ">tugga</span> eller <span class="alternativ">slänga</span> potatisen?</span> ';
+        questionBox.innerHTML = '<span class="text">Du valde potatis! Mycket bra val!, joddlade fräsen. <br><br> Innan du tar en tugga vill jag att du ska veta, att om du går vilse, ropa bara <span class="alternativ">hem</span>. <br> <br>Vill du ta en <span class="alternativ">tugga</span> eller <span class="alternativ">slänga</span> potatisen?</span> ';
 
 
     }
@@ -58,37 +64,76 @@ function submit() {
     if (answer.value == 'tugga') {
         answer.value = '';
 
-        questionBox.innerHTML = '<span class="text"> TJOLLAHOPP!! Det var en teleporteringspotatis! <br> Du befinner dig nu på en åker. Det dundrar i horisonten, något stort är påväg. <br> Helt plötsligt står en stor gorillaliknande potatis framför dig. Den store Blå King Kongo. <br><br> Vad vill du göra? <br> <br> <span class="alternativ"> Fly </span> eller <span class="alternativ">Fläkta</span>'
+        questionBox.innerHTML = '<span class="text"> TJOLLAHOPP!! Den här potatisen var full av teleporteringsjuice!<br> Du befinner dig nu på en åker. Det dundrar i horisonten, något stort är påväg. <br> Helt plötsligt står en stor gorillaliknande potatis framför dig. Den store Blå King Kongo. <br><br> Vad vill du göra? <br> <br> <span class="alternativ"> Fly</span>, kika i <span class="alternativ"> fickan </span>eller <span class="alternativ">fläkta</span>'
 
     }
 
+    if (answer.value == 'slänga') {
+        answer.value = '';
+
+        questionBox.innerHTML = '<span class="text"> Du slänger ut potatisen från sockervaddsmolnet och ser den försvinna. <br><br> Olyckligtvis är det en boomerang-potatis som kommer tillbaka i en väldig fart och träffar dig i huvudet :( </span>'
+
+        // setInterval(function(){ alert('GAME OVER'); 
+        //     ; }, 4000);
+
+        setInterval(function () {
+            alert('GAME OVER');
+            window.location.reload(true);;
+        }, 6000);
+
+
+    }
 
     if (answer.value == 'fly') {
         answer.value = '';
 
-        questionBox.innerHTML = '<span class="text"> Du flyr snabbt som vinden. Lite för snabbt så du ser inte riktigt vart du är påväg. <br><br> KRASH! Du springer rakt in i en ladugård, i sån hastighet att den exploderar. <br><br> Kvar finns endast en <span class="alternativ"> potatisskalare</span> och ett paket <span class="alternativ">vispgrädde</span> </span>'
+        questionBox.innerHTML = '<span class="text"> Du flyr snabbt som vinden. Lite för snabbt så du ser inte riktigt vart du är påväg. <br><br> KRASH! Du springer rakt in i en ladugård, i sån hastighet att den exploderar. <br><br> Kvar finns endast en <span class="alternativ"> färsk fesk</span> Vill du ta upp den? <br><br> <span class="alternativ"> Ja / Nej </span></span>'
 
 
     }
 
-    if (answer.value == 'skalare') {
+    if (answer.value == 'ja') {
         answer.value = '';
-        items.push('potatisskalare');
+        items.push('fisk');
 
-        questionBox.innerHTML = 'Detta funkar'
+        alert('Du har nu en färsk fesk i fickan');
+        questionBox.innerHTML = 'BRAAAK!! Nu har Den stora Blå King Kongo kommit ikapp.'
 
         console.log(items);
     }
 
+    if (answer.value == 'fläkta') {
+        answer.value = '';
 
-
-
-
-
-
-    if (answer.value == 'ryggsäck') {
-        questionBox.innerHTML = '<span class="text">Föremål i fickan:</span><br><br>' + items;
+        questionBox.innerHTML = '';
     }
+
+
+
+    if (answer.value == 'fickan') {
+        answer.value = '';
+
+        for (let i = 0; i < items.length; i++) {
+            if (items[i] == 'färsk fesk') {
+                prompt('Du har nu en färsk fesk i fickan!')
+
+                i
+
+            } else {
+                prompt('Du har bara ludd i fickan..')
+
+            }
+
+        }
+    }
+
+  
+
+
+
+
+
+
 
 
 }
