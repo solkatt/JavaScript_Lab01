34902f64b8ef2dd2632511fc081fbece5a8c2c0a
Viktor Ödman <viktoralexanderodman@gmail.com>
2019-11-27 23:22:10 +0100

    readMe added description. Fixes for html, css, javacript

--- README.md
+++ README.md
@@ -1,12 +1,80 @@
-# JavaScript_Lab01
- Interactive javaScript 
+# JavaScript Lab I
 
-Descripiton 
+Målet med detta projekt är att skapa en interaktiv upplevelse med hjälp av HTML, CSS och JavaScript. 
 
-
-
-
-Features
+Användaren får vid varje händelse två val som leder till olika vägar. Vissa val för berättelsen framåt medans vissa val leder till *Game Over*. 
 
 
+## Metoder
+
+### HTML
+#### Exempel på kod
+
+Upplevelsen förmedlas via text, där händelser, dialoger och val presenteras. 
+
+```
+<div id="questionBox">
+    <p class="text" id="info">
+        <span class="text">Här visas information om handling och val. 
+    </p>
+</div>
+```
+
+Användaren interagerar med hjälp av en input av typen text.
+Där användaren får skriva in vilket val de vill göra samt trycka på *Submit-knappen* eller Enter-tangenten. 
+
+```
+<div id="answerBox">
+    <input type="text" id="answer"/> 
+    <button onclick="submit();" >Submit</button>
+
+</div>
+```
+
+### JavaScript
+#### Exempel på kod
+
+Interaktionen fungerar med hjälp av ett stort gäng *if statements*. 
+ 
+```
+if (answer.value == 'Användarens svar') {
+        answer.value = '';
+        questionBox.innerHTML = '<span class="text">Texten uppdateras beroende på svar </span>'
+    }
+```
+
+Vid vissa tilffällen fördröjs uppdateringen, t.ex. när man vinner/förlorar eller flera uppdateringar av text ska ske efter varandra.
+
+```
+        setTimeout(function () {
+            alert('GAME OVER');
+            window.location.reload(true);;
+        }, 6000);
+    }
+```
+
+För att göra det möjligt för användaren att *submitta* sitt svar med Enter-tangenten skrevs följande kod.
+
+```
+document.onkeydown = function () {
+    if (window.event.keyCode == '13') {
+        submit();
+    }
+}
+```
+
+## Avgränsingar
+### Exempel på avgränsingar
+Ambitionen var från början att göra det möjligt för användaren att plocka upp flera objekt och spara dem i en digital ryggsäck. I nuläget finns ett objekt möjligt att plocka upp, som sparas i en *array*. 
+
+Planen var också att visa olika bilder ut genom spelet, men dessa två *feautures* är tyvärr inte klara. Därför finns det spår av dem i koden.
+
+
+
+## Länkar
+
+[GitHub Repo](https://github.com/solkatt/JavaScript_Lab01.git)
+
+
+[Demo](https://github.com/solkatt/JavaScript_Lab01.git)
 
--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// // // // PLACE load ASCI IMAGE
-
-let userData = []
-
-
-
-
-
 
 const userAnswer = document.getElementById('answer');
 
@@ -13,33 +5,21 @@ const info = document.querySelector('#info h2');
 let snorkelSound = new Audio('/sound/sax.mp3');
 snorkelSound.loop = false;
 
+// ENTER TRYCK
 
-
+document.onkeydown = function () {
+    if (window.event.keyCode == '13') {
+        submit();
+    }
+}
 
 // QUESTION BOX - INFO, ALTERNATIV, FRÅGOR
 
-
-
-
-
-//Ryggsäck Items -- features = spara, slänga och använda items
+// Ryggsäck Items 
 let items = [];
-// // // // Tidigare Val
-
-
-// UPPDATERA QUESTIONBOX
-function updateText() {}
-
-
-
-
 
 function submit() {
     answer.value = answer.value.toLowerCase();
-    // alert(answer.value);
-
-
-
 
     if (answer.value == 'start') {
         answer.value = '';
@@ -50,180 +30,93 @@ function submit() {
 
     if (answer.value == 'potatis') {
         answer.value = '';
-
-
         questionBox.innerHTML = '<span class="text">Du valde potatis! Mycket bra val!, joddlade fräsen. <br><br><br> <br>Vill du ta en <span class="alternativ">tugga</span> eller <span class="alternativ">slänga</span> potatisen?</span> ';
-
-
     }
 
     if (answer.value == 'tugga') {
         answer.value = '';
-
         questionBox.innerHTML = '<span class="text"> TJOLLAHOPP!! Den här potatisen var full av teleporteringsjuice!<br> Du befinner dig nu på en åker. Det dundrar i horisonten, något stort är påväg. <br> Helt plötsligt står en stor gorillaliknande potatis framför dig. Den store Blå King Kongo. <br><br> Vad vill du göra? <br> <br> <span class="alternativ"> Fly </span>eller <span class="alternativ">fläkta</span>'
-
     }
 
     if (answer.value == 'slänga') {
         answer.value = '';
-
         questionBox.innerHTML = '<span class="text"> Du slänger ut potatisen från sockervaddsmolnet och ser den försvinna. <br><br> Olyckligtvis är det en boomerang-potatis som kommer tillbaka i en väldig fart och träffar dig i huvudet :( </span>'
-
-
-        setInterval(function () {
+        setTimeout(function () {
             alert('GAME OVER');
             window.location.reload(true);;
         }, 6000);
-
-
     }
 
     if (answer.value == 'fly') {
         answer.value = '';
-
         questionBox.innerHTML = '<span class="text"> Du flyr snabbt som vinden. Lite för snabbt så du ser inte riktigt vart du är påväg. <br><br> KRASH! Du springer rakt in i en ladugård, i sån hastighet att den exploderar. <br><br> Kvar finns endast en färsk fesk Vill du ta upp den? <br><br> <span class="alternativ"> Ja / Nej </span></span>'
-
-
     }
 
     if (answer.value == 'ja') {
         answer.value = '';
         items.push('fisk');
-
         alert('Du har nu en färsk fesk i fickan');
         questionBox.innerHTML = '<span class="text">BRAAAK!! Nu har Den stora Blå King Kongo kommit ikapp.  Han slungar dig upp tillbaka till sockervaddsmolnet!</span><span class="text"> Tillbaka på sockervaddsmolnet! Vill du prova <span class="alternativ">snorkel?</span></span>'
-
-
-
-
     }
-
 
     if (answer.value == 'nej') {
         answer.value = '';
 
         questionBox.innerHTML = '<span class="text">BRAAAK!! Nu har Den stora Blå King Kongo kommit ikapp. Han slungar dig upp tillbaka till sockervaddsmolnet!</span>'
 
-        setInterval(function () {
+        setTimeout(function () {
             questionBox.innerHTML = '<span class="text"> Tillbaka på sockervaddsmolnet! Vill du prova <span class="alternativ">snorkel?</span></span>'
         }, 6000);
     }
 
     if (answer.value == 'fläkta') {
         answer.value = '';
-
-        questionBox.innerHTML = '<span class="text"> Du fläktar med armarna och flyger upp till sockervaddsmolnet. <br><br>Tillbaka på sockervaddsmolnet! Vill du prova <span class="alternativ">snorkel?</span></span>';
+        questionBox.innerHTML = '<span class="text"> Du fläktar med armarna och flyger upp till sockervaddsmolnet. <br><br>Tillbaka på sockervaddsmolnet! Vill du prova <span class="alternativ">snorkel </span></span>';
     }
 
+    // DET STORA SNORKEL ÄVENTYRET
 
-
-    if (answer.value == 'fickan') {
-        answer.value = '';
-
-        for (let i = 0; i < items.length; i++) {
-            if (items[i] == 'fisk') {
-                prompt('Du har nu en färsk fesk i fickan!')
-
-
-
-            } else {
-                prompt('Du har bara ludd i denna fickan..')
-
-            }
-
-        }
-    }
-
-
-
-
-    // SNORKEL ÄVENTYRET
     if (answer.value == 'snorkel') {
         answer.value = '';
-
         questionBox.innerHTML = '<span class="text">GRATTIS! En alldelles egen snorkel! <br><br> Vill du <span class="alternativ">blåsa</span> eller <span class="alternativ">andas </span>i den? </span>'
     }
 
-
     if (answer.value == 'blåsa') {
         answer.value = '';
-
         snorkelSound.play();
-
         questionBox.innerHTML = '<span class="text">Det magiska ljudet som kom ur snorkel innehöll en såpass hög dos av jazz att du somnade. </span>'
-
-
-
-        setInterval(function () {
+        setTimeout(function () {
             alert('GAME OVER');
             window.location.reload(true);;
         }, 6000);
-
-
     }
 
     if (answer.value == 'andas') {
         answer.value = '';
-
-        questionBox.innerHTML = '<span class="text">VIPPS! Snorkeln var förtrollad! <br><br> Du befinner dig nu i en undervattenträdgård! <br><br> En bläckfisk smyger sig upp och frågar: <br><br> Har du nån <span class="alternativ">färsk fesk?</span></span>'
+        questionBox.innerHTML = '<span class="text">VIPPS! Snorkeln var förtrollad! <br><br> Du befinner dig nu i en undervattenträdgård! <br><br> En bläckfisk smyger sig upp och frågar: <br><br> Har du nån <span class="alternativ">färsk fesk </span> eller <span class="alternativ">icke?</span></span>'
     }
+
     if (answer.value == 'färsk fesk') {
         answer.value = '';
-
         for (let i = 0; i < items.length; i++) {
-            if (items[i] == 'fisk') {
-
-                questionBox.innerHTML = ' TACK FÖR FESKEN! DU VANN!'
-                setInterval(function () {
+            if (items[i] == 'fisk')  {
+                questionBox.innerHTML = '<span class="text">ÅÅÅH TACK FÖR DEN FÄRSKA FESKEN!! </span>'
+                setTimeout(function () {
                     alert('YOU WON!');
                     window.location.reload(true);;
-                }, 6000);
-
+                }, 6000);         
             }
         }
-        questionBox.innerHTML = '<span class="text">Du har ingen fesk!, bubblar bläckfisken och kastar dig tillbaka till sockervaddsmolnet!</span> <br><br><span class="text"> Tillbaka på sockervaddsmolnet! Vill du prova <span class="alternativ">potatis?</span></span>'
-
-
     }
 
+    if (answer.value == 'icke') {
+        answer.value = '';
+               console.log('Det funkar');
+               questionBox.innerHTML = '<span class="text">Du har ingen fesk!, bubblar bläckfisken och kastar dig tillbaka till sockervaddsmolnet!</span> <br><br><span class="text"> Tillbaka på sockervaddsmolnet! Vill du prova <span class="alternativ">potatis?</span></span>'
+                
+            }
 
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ENTER TRYCK
-
-document.onkeydown = function () {
-    if (window.event.keyCode == '13') {
-        // answer.style.backgroundColor = 'red';
-        submit();
-
-    }
-
-
+    else {
+        answer.value = '';
+    } 
 }
